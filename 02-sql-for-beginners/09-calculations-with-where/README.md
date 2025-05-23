22d183c0cbb7c2b56a5509580f75079a16126e17
Ian Johnson <tacoda@hey.com>
2025-05-23 13:40:19 +0200

    Add where calculations lesson

---
+++ 02-sql-for-beginners/09-calculations-with-where/README.md
@@ -0,0 +1,5 @@
+# Calculations with Where
+
+```sh
+psql -U ianjohnson -d storefront -f where_calculations.sql
+```
---
+++ 02-sql-for-beginners/09-calculations-with-where/where_calculations.sql
@@ -0,0 +1,2 @@
+SELECT * FROM products WHERE price * max_discount < 5.00;
+SELECT * FROM products WHERE price * max_discount >= 10.00;
